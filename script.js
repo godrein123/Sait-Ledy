39ebf353329aaf9143948968f81c3263e0d49387
Yurii Chebotarev <yuramba123@mail.ru>
2025-03-19 11:46:43 +1000

    создание липкой понели навигации, конечная версия

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+const parent = document.getElementById('HeadId');
+const chaild = document.getElementById('contChaild');
+const stickyDivBG = document.querySelector('conteaner');
 const stickyDiv = document.querySelector('.header_nav'); // Выбираем необходимый div
-const stickyPoint = stickyDiv.offsetTop; // Точка, при достижении которой элемент становится фиксированным
+const stickyPoint = stickyDiv.offsetHeight; // Точка, при достижении которой элемент становится фиксированным
 
 window.addEventListener('scroll', () => {
-  if (window.pageYOffset >= stickyPoint) {
+  if (window.pageYOffset > stickyPoint) {
     stickyDiv.classList.add('nav_bar_fixtTop'); // Фиксируем элемент, когда прокрутили до нужной точки
+    chaild.classList.remove('conteaner');
   } else {
     stickyDiv.classList.remove('nav_bar_fixtTop'); // Освобождаем его, если скролл находится выше этой точки
+    chaild.classList.add('conteaner');
   }
-});
+}); 
